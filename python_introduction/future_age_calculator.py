dd7b6c083eeeae25a85e0ab0db03a395d188d52c
godsent <godsentojugo@gmail.com>
2025-09-13 20:04:02 +0100

    Add future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+#future age calculator
+current_age = 10
+years_to_add = 25
+future_age = current_age + years_to_add
+print("In 2050 you will be", future_age, "years old.")
+#output: In 2050 you will be 35 years old.

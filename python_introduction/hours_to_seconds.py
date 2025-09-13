c402f973131b9b80ead73d1de63a9e9c3de9e6db
godsent <godsentojugo@gmail.com>
2025-09-13 19:29:27 +0100

    Add hours_to_seconds.py

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+#hours to seconds
+hours = 2
+seconds = hours * 3600
+print("2 hours is equal to", seconds, "seconds")
+#output: 2 hours is equal to 7200 seconds
+# The formula used is: seconds = hours * 3600

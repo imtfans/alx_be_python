47ef622e98fd70ab34784897effc78957532ffcf
godsent <godsentojugo@gmail.com>
2025-09-13 20:10:30 +0100

    Add modified future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,6 +1,6 @@
 #future age calculator
-current_age = 10
-years_to_add = 25
+current_age = 27
+years_to_add = 30
 future_age = current_age + years_to_add
 print("In 2050 you will be", future_age, "years old.")
-#output: In 2050 you will be 35 years old.
+#output: In 2050 you will be 57 years old.

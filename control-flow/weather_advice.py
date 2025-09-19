c267ed04f42ec63d01e674913b8e7077ced9ec51
godsent <godsentojugo@gmail.com>
2025-09-19 20:24:45 +0100

    fix typo in wather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,10 +2,12 @@
 weather = input("What's the weather like today? (sunny, rainy, cold) ")
 # provide advice based on the weather
 if weather == "sunny":
-    print("wear a t-shirt and sunglasses!")
+    print("Wear a t-shirt and sunglasses!")
 elif weather == "rainy":
-    print("dont forget your umbrella and raincoat.")
+    print("Don't forget your umbrella and raincoat.")
 elif weather == "cold":
-    print("make sure to wear a warm coat and a scarf.")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry, I don't have advice for that this weather.")
+    print("Sorry, I don't have advice for this weather.")
+
+    

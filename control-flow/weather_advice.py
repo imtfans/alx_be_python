809e92555d4f9b110bfb6d7a5113d341ac73f4c2
Godsent Omo-ojugo <godsentojugo@gmail.com>
2025-09-20 11:55:37 +0100

    fix error on  weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,13 +2,9 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 weather = weather.lower()
 # provide advice based on the weather
-if weather == "sunny":
-    print("Wear a t-shirt and sunglasses")
-elif weather == "rainy":
-    print("Don't forget your umbrella and raincoat.")
-elif weather == "cold":
-    print("Make sure to wear a warm coat and a scarf.")
-else:
-    print("Sorry, I don't have advice for this weather.")
+if weather == "sunny": print("Wear a t-shirt and sunglasses")
+elif weather == "rainy": print("Don't forget your umbrella and raincoat.")
+elif weather == "cold": print("Make sure to wear a warm coat and a scarf.")
+else: print("Sorry, I don't have advice for this weather.")
 
     

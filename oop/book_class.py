22afcd5d3a1e33c7a07d975e3152427ac5b94393
godsent <godsentojugo@gmail.com>
2025-10-11 12:36:06 +0100

    fix input on book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,21 +1,14 @@
-# book_class.py
-
 class Book:
     def __init__(self, title, author, year):
-        """Constructor that initializes book details"""
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book created: {self.title} by {self.author}, {self.year}")
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
 
     def __str__(self):
-        """User-friendly string representation"""
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        """Official developer representation"""
         return f"Book('{self.title}', '{self.author}', {self.year})"
-
-    def __del__(self):
-        """Destructor message when object is deleted"""
-        print(f"Deleting {self.title}")
